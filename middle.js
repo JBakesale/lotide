9939b23d92c7748e995c9b306c900a0c86788e36
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-03 16:57:32 -0700

    middle refactored and exported

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-}
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays (actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArray = require('./eqArray');
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(array) {
   const length = array.length;
@@ -34,10 +15,4 @@ const middle = function(array) {
   return [array[middleIndex -1], array[middleIndex]];
 };
 
-let result = middle([1, 2])
-console.log(result);
-
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
