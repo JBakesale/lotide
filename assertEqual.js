90116f78eb661850fdad0f7d69557f466b1d9c47
Joshua Baker <joshuadfbaker@gmail.com>
2023-07-03 15:59:23 -0700

    refactored with module exports

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,5 @@ const assertEqual = function(actual, expected) {
   console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
-    
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("jones", "Jones");
-assertEqual(4567, 4567);
+module.exports = assertEqual;
+
